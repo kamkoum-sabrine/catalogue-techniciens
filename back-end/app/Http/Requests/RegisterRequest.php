f64dff7ccf74aff6c93377fb1bcbecf52f5b0b48
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-06-28 23:43:18 +0200

    [Laravel] create RegisterRequest

---
+++ back-end/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    // /**
+    //  * Determine if the user is authorized to make this request.
+    //  *
+    //  * @return bool
+    //  */
+    // public function authorize()
+    // {
+    //     return false;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name'=>'required|string|max:255',
+            'last_name'=>'required|string|max:255',
+            'cin'=>'integer|digits:8|unique:users',
+            'birth_date'=>'required|date',
+            'phone_number'=>'required|integer|digits:8',
+            'email'=>'required|string|email|max:255|unique:users',
+            'adresse'=>'required|string|max:255|unique:users',
+            'password'=>'required|string|min:8|confirmed',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+}
