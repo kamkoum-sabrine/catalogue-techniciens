ba9e25f9bdcc5fccae82e9d700682006d450624b
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-06-29 00:40:40 +0200

    [Vue] import and add interceptor in main.js

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import "./plugins/axios";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import interceptorsSetup from './helpers/interceptors';
 
 Vue.config.productionTip = false;
-
+interceptorsSetup()
 new Vue({
   vuetify,
   render: (h) => h(App),
