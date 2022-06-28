505b2d18fafb05a6d44148b7be811be24a06565f
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-06-29 00:24:20 +0200

    [Laravel] UserFactory, user table and seeder

--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('cin');
+            $table->date('birth_date');
+            $table->integer('phone_number');
+            $table->string('adresse');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
