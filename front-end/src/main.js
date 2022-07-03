fc939837ebed96204f931f230a008d5d4111b47e
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-07-03 12:51:36 +0200

    [Vue] reinit vue project

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,12 +1,8 @@
-import Vue from "vue";
-import "./plugins/axios";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import interceptorsSetup from './helpers/interceptors';
+import Vue from 'vue'
+import App from './App.vue'
 
-Vue.config.productionTip = false;
-interceptorsSetup()
+Vue.config.productionTip = false
+
 new Vue({
-  vuetify,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app')
