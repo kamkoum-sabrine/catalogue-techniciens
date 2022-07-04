3e3d793daf30f6cbbf0d438f04dae356a02e4945
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-07-04 16:18:02 +0200

    [Vue] add vuex

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -5,9 +5,10 @@ import './plugins/axios'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import interceptorsSetup from './helpers/interceptors';
 
 Vue.config.productionTip = false
-
+interceptorsSetup()
 new Vue({
   vuetify,
   render: h => h(App)
