f88fcccb98f06b028e5dda22b3c2537b32fd6f43
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-07-18 23:58:48 +0200

    [Vue] add form to create specialite

--- front-end/src/components/gererSpecialite.vue
+++ front-end/src/components/gererSpecialite.vue
@@ -1,27 +1,48 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">Nom Specialite</th>
+  <div class="container">
+    <v-form ref="form" @submit.prevent="ajouterSpecialite">
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        label="Nom spécialité"
+        required
+      ></v-text-field>
 
-          <th class="text-left">Action</th>
-          <th class="text-left">Sous specialites</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in specialites" :key="item.id">
-          <td>{{ item.name }}</td>
-          <td>
-            <!-- <tr v-for="i in item.sous_specialite" :key="i.id">
+      <v-btn color="#7CB342" class="mr-4"> Validate </v-btn>
+    </v-form>
+    <br />
+    <br />
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">Nom Specialite</th>
+
+            <th class="text-left">Action</th>
+            <th class="text-left">Sous specialites</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in specialites" :key="item.id">
+            <td>{{ item.name }}</td>
+            <td>
+              <!-- <tr v-for="i in item.sous_specialite" :key="i.id">
               <td>{{ i.name }}</td>
             </tr> -->
-            <button @click="deleteSpecialite(item.id)"></button>
-          </td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+              <!-- <button @click="deleteSpecialite(item.id)"></button> -->
+              <v-btn
+                color="#F48FB1"
+                class="mr-4"
+                @click="deleteSpecialite(item.id)"
+              >
+                Validate
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
 <script>
 export default {
@@ -29,6 +50,7 @@ export default {
   data() {
     return {
       specialites: [],
+      name: "",
     };
   },
   created() {
@@ -42,7 +64,7 @@ export default {
   methods: {
     deleteSpecialite(id) {
       this.$http
-        .get("http://localhost:8000/api/delete/" + id)
+        .delete("http://localhost:8000/api/specialites/delete/" + id)
         .then((response) => {
           console.log(response.data);
           this.specialites = response.data;
