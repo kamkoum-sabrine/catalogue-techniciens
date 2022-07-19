25f9feb79dc0e05f28e3e1f1c979121c011ed512
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-07-19 12:14:03 +0200

    [Laravel] seeding 3 clients

--- back-end/database/seeders/UserSeeder.php
+++ back-end/database/seeders/UserSeeder.php
@@ -51,6 +51,42 @@ class UserSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ]);
+        \App\Models\User::create([
+            'first_name' => "Ahmed",
+            'last_name' => "Ben Admed",
+            'cin' => rand(10000000,99999999),
+            'phone_number' => rand(10000000,99999999),
+            'birth_date' =>null,
+            'adresse' => Str::random(5),
+            'email' => "ahmedbenahmed@example.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        \App\Models\User::create([
+            'first_name' => "Sabrine",
+            'last_name' => "Kamkoum",
+            'cin' => rand(10000000,99999999),
+            'phone_number' => rand(10000000,99999999),
+            'birth_date' =>null,
+            'adresse' => Str::random(5),
+            'email' => "sabrinekamkoum@example.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        \App\Models\User::create([
+            'first_name' => "Mohamed",
+            'last_name' => "Ben Salah",
+            'cin' => rand(10000000,99999999),
+            'phone_number' => rand(10000000,99999999),
+            'birth_date' =>null,
+            'adresse' => Str::random(5),
+            'email' => "mohamedbensalah@example.com",
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
         // DB::insert('insert into role_user (user_id, role_id) values (?, ?)', [1, 1]);
 
     }
