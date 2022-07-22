8cbf857eccabefc3c5348db029f284f9022e90a8
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-07-22 11:51:36 +0200

    [Laravel] Rate model and relationships

--- back-end/app/Models/Rate.php
+++ back-end/app/Models/Rate.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Rate extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        "client_id",
+        "prestataire_id",
+        "note"
+
+    ];
+    public function prestataire(){
+        return $this->hasOne(User::class,'id','prestataire');
+    }
+    public function client(){
+        return $this->hasOne(User::class,'id','client');
+    }
 }
