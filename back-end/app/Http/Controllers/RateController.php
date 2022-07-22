ed7d9f048aaa98029ded4c216dc3e4bc0c574a39
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-07-22 12:00:54 +0200

    [Laravel] create function show notes in controller

--- back-end/app/Http/Controllers/RateController.php
+++ back-end/app/Http/Controllers/RateController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rate;
 use Illuminate\Http\Request;
 
 class RateController extends Controller
 {
-    //
+    public function show(){
+        $rates = Rate::all();
+        if (empty($rates)) {
+            return response()->json(['message' => 'Aucun Rate n est disponible! '], 404);
+        }
+        return response()->json($rates, 200);
+    }
+
+    
 }
